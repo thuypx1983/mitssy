2f5b5a2dfa39eae2a6f053d6b447a8f41ed8cefc
Pham Xuan Thuy <phamxuanthuy@sutunam.com>
2017-10-11 11:30:55 +0700

    mew udpate

--- sites/all/themes/oms/templates/html.tpl.php
+++ sites/all/themes/oms/templates/html.tpl.php
@@ -14,5 +14,20 @@
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
+
+  <!-- google plus -->
+  <script src="https://apis.google.com/js/platform.js" async defer></script>
+  <!-- !google plus -->
+
+  <!-- facebook comment -->
+  <div id="fb-root"></div>
+  <script>(function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.10";
+          fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+  <!-- !facebook comment -->
 </body>
 </html>
