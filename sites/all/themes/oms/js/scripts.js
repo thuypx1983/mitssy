674c916526449c24f5c0adea77ff0c41b28f1945
Pham Xuan Thuy <phamxuanthuy@sutunam.com>
2017-11-14 14:55:07 +0700

    a

--- sites/all/themes/oms/js/scripts.js
+++ sites/all/themes/oms/js/scripts.js
@@ -56,6 +56,15 @@
                 slidesToScroll: 4,
                 dots:true
             });
+            $('.partner .view-content').slick({
+                autoplay: false,
+                arrows: true,
+                dots:false,
+                infinite: true,
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                dots:true
+            });
         },
 
         autoHeight:function(){
